5b8bc6298d696f6ffbec4d2ac095daf1c945f1de
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-26 19:40:43 -0300

    Feat: Requisito 2 incompleto, falta corrigir terceiro teste

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,8 +1,56 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import About from '../pages/About';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-// describe('Requisito 2 - Componente <About.js />, () => {
-//   test('',
-//     () => {});
-// });
+describe('Requisito 2 - Componente <About.js />', () => {
+  test('Testa se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', { name: 'About' });
+    userEvent.click(about);
+
+    const expected = screen.getByText(/This application simulates a Pokédex/i);
+    expect(expected).toBeInTheDocument();
+  });
+
+  test('se a página contém um heading h2 com o texto /About Pokédex/', () => {
+    renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', { name: 'About' });
+    userEvent.click(about);
+
+    const expected = screen.getByRole('heading', {
+      name: /About Pokédex/i,
+      level: 2,
+    });
+
+    expect(expected).toBeInTheDocument();
+  });
+
+  // test('se a página contém dois parágrafos com texto sobre a Pokédex', () => {
+  // renderWithRouter(<App />);
+
+  // const about = screen.getByRole('link', { name: 'About' });
+  // userEvent.click(about);
+
+  // const expected = screen.getAllByText(/Pokémons/i); // erro, tem que ser Pokémons especificamente dentro de uma tag <p> se não pega dento do link tbm
+
+  // expect(expected).toHaveLength(2);
+  // });
+
+  test('se a página contém a imagem esperada de uma Pokédex', () => {
+    renderWithRouter(<App />);
+
+    const about = screen.getByRole('link', { name: 'About' });
+    userEvent.click(about);
+
+    const expected = screen.getByRole('heading', { name: 'About Pokédex', level: 2 });
+    expect(expected).toBeInTheDocument();
+
+    const URL = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+    const expectedImage = screen.getByAltText('Pokédex');
+    expect(expectedImage.src).toBe(URL);
+  });
+});
