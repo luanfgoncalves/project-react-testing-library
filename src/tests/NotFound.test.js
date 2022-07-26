950e8b14c25d095d2eacf2a415397c96a9cc3d4b
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-26 10:36:02 -0300

    Feat: Requisito 4 completo, testes para NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Requisito 4 - Componente <NotFound.js />', () => {
+  test('se a página contém um heading h2 com o texto /Page requested not found/',
+    () => {
+      render(<NotFound />);
+      const title = screen.getByRole('heading', {
+        name: /Page requested not found/i,
+        level: 2,
+      });
+
+      expect(title).toBeInTheDocument();
+    });
+
+  test('se ao renderizar, a página mostra a imagem de um pikachu chorando', () => {
+    render(<NotFound />);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const alt = 'Pikachu crying because the page requested was not found';
+    const image = screen.getByAltText(alt);
+
+    expect(image).toHaveAttribute('src', url);
+  });
+});
