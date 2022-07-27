fcd3be9e4bcafd28b2a727c35274791463ac9154
luanfgoncalves <luanfgoncalves@outlook.com>
2022-07-26 21:46:45 -0300

    Feat: Requisito 3 completo

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,8 +1,81 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import FavoritePokemons from '../pages/FavoritePokemons';
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import App from '../App';
 
-// describe('Requisito 3 - Componente <FavoritePokemons.js />, () => {
-//   test('',
-//     () => {});
-// });
+const NO_FAVORITES = 'No favorite pokemon found';
+
+const FAVORITED = 'Pokémon favoritado?';
+
+const MARKED_FAVORITE = 'Pikachu is marked as favorite';
+
+const FAVORITE = 'Favorite Pokémons';
+
+const MORE_DETAILS = 'More details';
+
+describe('Requisito 3 - Componente <FavoritePokemons.js />', () => {
+  test('se é exibida o texto /No favorite pokemon found/, na ausência pokémons favoritos',
+    () => {
+      renderWithRouter(<App />);
+
+      const favorites = screen.getByRole('link', { name: FAVORITE });
+
+      userEvent.click(favorites);
+
+      const noFavorite = screen.getByText(NO_FAVORITES);
+
+      expect(noFavorite).toBeInTheDocument();
+    });
+
+  test('se é exibido o card do pokémon favoritado.',
+    () => {
+      renderWithRouter(<App />);
+
+      const favorites = screen.getByRole('link', { name: FAVORITE });
+
+      // favorita Pikachu
+      const moreDetails = screen.getByRole('link', { name: MORE_DETAILS });
+
+      userEvent.click(moreDetails);
+
+      const favoriteCheck = screen.getByLabelText(FAVORITED);
+
+      userEvent.click(favoriteCheck);
+
+      userEvent.click(favorites);
+
+      const markedAsFavorite = screen.getByAltText(MARKED_FAVORITE);
+
+      expect(markedAsFavorite).toBeInTheDocument();
+    });
+
+  test('se não é exibido o card do pokémon desfavoritado.',
+    () => {
+      renderWithRouter(<App />);
+
+      const favorites = screen.getByRole('link', { name: FAVORITE });
+
+      // favorita Pikachu
+      const moreDetails = screen.getByRole('link', { name: MORE_DETAILS });
+
+      userEvent.click(moreDetails);
+
+      const favoriteCheck = screen.getByLabelText(FAVORITED);
+
+      userEvent.click(favoriteCheck);
+
+      userEvent.click(favorites);
+
+      const noFavorite = screen.getByText(NO_FAVORITES);
+
+      // desfavorita Pikachu
+      userEvent.click(moreDetails);
+
+      userEvent.click(favoriteCheck);
+
+      userEvent.click(favorites);
+
+      expect(noFavorite).toBeInTheDocument();
+    });
+});
